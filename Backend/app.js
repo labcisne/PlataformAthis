5cd452578c7d101b3e05f6f051c42365523d7d59
GustavooPerini <gustavooperini@gmail.com>
2025-03-31 09:49:57 -0300

    Diretório para imagens e arquivos gerais são criados ao iniciar o programa

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,8 @@ const optionsRoute = require('./Routes/optionsRoute');
 const globalErrorHandler = require('./Controllers/errorController');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const fs = require('node:fs');
+const path = require('node:path');
 
 
 const app = express();
@@ -20,6 +22,17 @@ app.use(cookieParser());
 app.use(authRoute);
 app.use('/familia', familyRoute);
 app.use('/options', optionsRoute);
+
+const imagensPath = path.join(__dirname, "imagens");
+const arquivosPath = path.join(__dirname, "arquivos");
+
+if(!fs.existsSync(imagensPath)){
+    fs.mkdirSync(imagensPath, { recursive: true })
+}
+
+if(!fs.existsSync(arquivosPath)){
+    fs.mkdirSync(arquivosPath, { recursive: true })
+}
 
 app.use('/familia/dadosFamilia/arquivosGerais/imagens', express.static('imagens'));
 app.use('/familia/dadosFamilia/arquivosGerais/arquivos', express.static('arquivos'));
